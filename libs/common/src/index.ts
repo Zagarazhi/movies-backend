e130d33a80c584b6a99e668dd5a50b532e710fbd
Zagarazhi <fukuroOwl@yandex.ru>
2023-04-18 03:21:42 +0400

    also to libs

---
+++ libs/common/src/index.ts
@@ -0,0 +1,6 @@
+export * from './movies-shared/country.model';
+export * from './movies-shared/genre.model';
+export * from './movies-shared/movie.model';
+export * from './movies-shared/dto/create-country.dto';
+export * from './movies-shared/dto/create-genre.dto';
+export * from './movies-shared/dto/create-movie.dto';
